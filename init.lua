67f13ebeb4525568d0deab915756e85fe99ef290
MrXnight <alexandre.benoit.riou@gmail.com>
2026-08-30 23:06:35 +0200

    Set indent to spaces

--- init.lua
+++ init.lua
@@ -107,6 +107,12 @@ vim.o.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
+-- space for tab
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
